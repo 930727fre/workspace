8d713781f494ece6c5ded1d68fc3c0bee0058f53
930727fre <930727fre@930727frem2.local>
2022-10-21 17:43:41 +0800

    trivial

---
+++ ccu-csie/10038.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n,i=0;
+    scanf("%d",&n);
+    int arr[n-1];
+    while(scanf("%d",&arr[i])!=EOF){
+        if(i==0){
+            continue;
+        }
+        else{
+            
+        }
+    }
+}
