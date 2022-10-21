2b3dcdc15f68113193642d551e7a69a595fce570
930727fre <930727fre@930727frem2.local>
2022-10-21 17:34:17 +0800

    trivial

--- 2022programming/test1.c
+++ 2022programming/test1.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    int arr[1000000];
-    for(int i=0;i<1000000;i++){
-        arr[i]=i;
-        printf("%d",arr[i]);
-    }
+    printf("%d %d",'0','9');
 }
---
+++ uva/11349.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    char c;
+    scanf("%d",&t);
+    while(scanf("%c",&c)){
+        
+    }
+}
+
