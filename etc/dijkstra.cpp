3a9adece6a6c0593abeafa191e05e2b6ee5edad8
frem2 <930727fre@gmail.com>
2023-03-07 10:48:48 +0800

    trivial

---
+++ etc/dijkstra.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long LL;
+vector<pair<LL,LL>> vec[10005];
+bool vis[10005];
+LL dis[10005];
+int main(){
+    int n,m,u,v,w;
+}
--- t.cpp
+++ t.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+
+int **fc(void){
+    int **matrix;
+    matrix=(int**)malloc(5*sizeof(int*));
+    matrix[0]=(int*)malloc(1);
+    matrix[0][0]=3;
+    return matrix;
+}
 int main(){
-    printf("%lld",-11);
+    int **matrix=fc();
+    printf("%d",matrix[0][0]);
+
+    
 }
