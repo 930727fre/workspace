e4120ff4a0780f97a7d09a756b79fdbf559917ae
ubuntu <930727fre@gmail.com>
2023-03-11 22:16:13 +0800

    trivial

---
+++ sp/Lab1_example/411410005.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+    printf("## ");
+    char buf[10000];
+    pid_t pid;
+    int status;
+
+    while(fgets(buf,10000,stdin)!=NULL){
+        printf("## ");
+        if(buf[strlen(buf)-1]=='\n'){
+            buf[strlen(buf)-1]='\0';
+        }
+        pid=fork();
+        if(pid<0){
+            exit(EXIT_FAILURE);
+        }
+        else{
+            if(pid==0){
+                execlp(buf, buf, (char *)0);
+            }
+            else{
+				if ((pid = waitpid(pid, &status, 0)) < 0)
+					printf("waitpid error");
+				printf("%% ");
+            }
+
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
