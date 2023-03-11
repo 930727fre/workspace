40d37faeddae3f78a11dd457f53a1e86288b04a4
ubuntu <930727fre@gmail.com>
2023-03-11 23:13:44 +0800

    "sp_lab01"

--- sp/Lab1_example/411410005.c
+++ sp/Lab1_example/411410005.c
@@ -3,29 +3,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int main(int argc, char *argv[]){
-    printf("## ");
+int main(){
     char buf[10000];
     pid_t pid;
     int status;
+    char *token, *arg[1000], *command;
+    int counter=0;
 
+    printf("## ");
     while(fgets(buf,10000,stdin)!=NULL){
-        printf("## ");
+        counter=0;
         if(buf[strlen(buf)-1]=='\n'){
             buf[strlen(buf)-1]='\0';
         }
+        token=strtok(buf," ");
+        command=token;
+        while(token!=NULL){
+            arg[counter]=token;
+            counter++;
+            token=strtok(NULL," ");
+        }
+        arg[counter]=NULL;
+
         pid=fork();
         if(pid<0){
             exit(EXIT_FAILURE);
         }
         else{
             if(pid==0){
-                execlp(buf, buf, (char *)0);
+                execvp(command,arg);
             }
             else{
 				if ((pid = waitpid(pid, &status, 0)) < 0)
 					printf("waitpid error");
-				printf("%% ");
+				printf("## ");
             }
 
         }
