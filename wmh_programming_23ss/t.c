b477fa7dbf0b1cd3ae32fc46855666a2a73b7ee4
ubuntu <930727fre@gmail.com>
2023-03-14 20:31:47 +0800

    trivial

--- wmh_programming_23ss/t.c
+++ wmh_programming_23ss/t.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
-long long ans=0;
+typedef long long LL;
+int arr[100];
+int ans=1;
+int n,e,quota;
+void sort(void);
+
 int main(){
-    int n,e,temp;
     scanf("%d %d",&n,&e);
     for(int i=0;i<n;i++){
-        scanf("%d",&temp);
-        ans+=temp;
+        scanf("%d",&arr[p]);
+        arr[p][1]=1;
     }
-    temp=ans%e;
-    ans/=e;
-    if(temp){
-        printf("%lld",ans+1);
+    sort();
+    quota=e;
+    for(int i=0;i<n;i++){
+        if(!arr[p]){
+            continue;
+        }
+        else{
+            if(quota>arr[p]){
+                quota-=arr[p];
+                arr[p]=0;
+            }
+        }
     }
-    else{
-        printf("%lld",ans);
+    printf("%d",ans);
+}
+
+void sort(void){
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
     }
+    return;
 }
--- wmh_programming_23ss/week04_q2_fater.c
+++ wmh_programming_23ss/week04_q2_fater.c
@@ -9,21 +9,32 @@ int main(){
     scanf("%d %d",&n,&e);
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
+        arr[i]=1;
     }
-
+    sort();
+    quota=e;
+    for(int i=0;i<n;i++){
+        if(quota>=arr[i]){
+            quota-=arr[i];
+        }
+        else{
+            quota=e-arr[i];
+            ans++;
+        }
+    }
     printf("%d",ans);
 }
 
-// void sort(void){
-//     int temp;
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<n-i-1;j++){
-//             if(arr[j]>arr[j+1]){
-//                 temp=arr[j];
-//                 arr[j]=arr[j+1];
-//                 arr[j+1]=temp;
-//             }
-//         }
-//     }
-//     return;
-// }
+void sort(void){
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return;
+}
