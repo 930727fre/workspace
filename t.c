254d2df73ee8a2250d76e4fe82a9142e76773f04
Freddy Chuang <930727fre@gmail.com>
2023-05-08 19:17:11 +0800

    trivial

--- t.c
+++ t.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-int main(){
-	perror("he");
-	exit(0);
-	while(1)
-}
+/html/body/div[3]/div/div[1]/div/div/div[1]/h1
+/html/body/div[3]/div/div[2]/div/div/div[1]/h1
+/html/body/div[3]/div/div[5]/div/div/div[1]/h1'
 
+/html/body/div[3]/div/div[5]/div/div/div[1]/pre 
