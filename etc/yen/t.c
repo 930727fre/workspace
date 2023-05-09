20aa9b23755913dc098916a6119ad5e352807a32
Freddy Chuang <930727fre@gmail.com>
2023-05-09 21:48:52 +0800

    trivial

--- etc/yen/t.c
+++ etc/yen/t.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
     char a[251], b[251], c[502];
-    int index,numA,numB, lenA, lenB;
-    for(int k=0;k<2;k++){
-        scanf("%s %s",a,b);
+    int numA,numB, lenA, lenB;
+    while(scanf("%s %s",a,b)!=EOF){
+
         lenA=strlen(a);
         lenB=strlen(b);
         for(int i=0;i<502;i++){
@@ -15,23 +16,23 @@ int main(){
             for(int j=lenB-1;j>=0;j--){
                 numA=a[i]-'0';
                 numB=b[j]-'0';
-                c[i+j]+=numA*numB;
-                c[i+j-1]+=c[i+j]/10;
-                c[i+j]%=10;
+                c[i+j+1]+=numA*numB;
+                c[i+j]+=c[i+j+1]/10;
+                c[i+j+1]%=10;
             }
         }
 
         for(int i=0;i<502;i++){
             c[i]+='0';
         }
-        if(c[0]==0){
+        if(c[0]=='0'){
             for(int i=0;i<500;i++){
-                c[i]=c[i+2];
+                c[i]=c[i+1];
             }
-            c[lenA+lenB]=0;
+            c[lenA+lenB-1]=0;
         }    
         else{
-            c[lenA+lenB-1]=0;
+            c[lenA+lenB]=0;
         }
         printf("%s\n",c);
     }
