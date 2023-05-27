dc920e0077442891562b70191d9057504bfc5c09
Freddy Chuang <930727fre@gmail.com>
2023-05-27 14:56:35 +0800

    trivial

--- sp/t.c
+++ sp/t.c
@@ -2,7 +2,10 @@
 
 int main() {
     char buffer[BUFSIZ];
+    char str[100];
     setvbuf(stdout, buffer, _IOLBF, BUFSIZ);
+    scanf("%s\n",str);
+    printf("%s",buffer);
 
     // Rest of the code...
 
