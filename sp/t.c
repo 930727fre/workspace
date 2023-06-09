0c594a2891ea12148858011c3a74489b15080cd6
frem2 <930727fre@gmail.com>
2023-06-09 22:53:13 +0800

    trivial

--- sp/t.c
+++ sp/t.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 int main() {
-    char buffer[BUFSIZ];
-    char str[100];
-    setvbuf(stdout, buffer, _IOLBF, BUFSIZ);
-    scanf("%s\n",str);
-    printf("%s",buffer);
+    const char* username = "930727fre";
+    struct passwd* user_info;
 
-    // Rest of the code...
+    user_info = getpwnam(username);
+    if (user_info == NULL) {
+        fprintf(stderr, "Failed to get user information for %s\n", username);
+        exit(1);
+    }
+
+    printf("Username: %s\n", user_info->pw_name);
+    printf("User ID: %d\n", user_info->pw_uid);
+    printf("Group ID: %d\n", user_info->pw_gid);
+    printf("Home directory: %s\n", user_info->pw_dir);
+    printf("Shell: %s\n", user_info->pw_shell);
 
     return 0;
 }
