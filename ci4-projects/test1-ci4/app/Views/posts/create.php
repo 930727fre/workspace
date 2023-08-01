7e0b4fe457f5aeb1972e76c559e21c3e933f6493
frem2 <930727fre@gmail.com>
2023-08-01 17:07:46 +0800

    trivial

---
+++ ci4-projects/test1-ci4/app/Views/posts/create.php
@@ -0,0 +1,10 @@
+<h1>new post</h1>
+<form action="/PostController/store" method="POST">
+    <h2>post title:</h2>
+    <input name="title">
+    <h2>post content:</h2>
+    <textarea name="content"></textarea>
+    <br>
+    <div style="height:20px"></div>
+    <button type="submit">submit</button>
+</form>
---
+++ ci4-projects/test1-ci4/app/Views/posts/show.php
@@ -0,0 +1,3 @@
+<h3><?php echo $posts['title']?></h3>
+<br>
+<h3><?php echo $posts['content']?></h3>
