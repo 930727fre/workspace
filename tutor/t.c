214bfc7c20ee58e8883d8d78dd5c6570d31ca44c
frem2 <930727fre@gmail.com>
2023-08-05 23:25:58 +0800

    trivial

--- tutor/t.c
+++ tutor/t.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-int pay(int);
-
 int main(){
-    int n;
-    printf("%d",n);
+    p=jolly[n]-jolly[n+1];
+    if(delta[p]==1){
+        printf("not Jolly\n");
+        break;
+    }
+    else{
+        delta[p]=1;
+    }
 }
 
 
-//abc
+//
+/*
+n=4
+input =1,3,4,7
+delta 
+
+n=4
+input =1,3,5
+delta 2 2
+delat[2]=1;
+*/
