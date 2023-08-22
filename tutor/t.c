ed8eaf7be4e19c4cf6d8e0f6660081771b60b69e
Freddy Chuang <930727fre@gmail.com>
2023-08-22 18:14:25 +0800

    trivial

--- tutor/t.c
+++ tutor/t.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
-    p=jolly[n]-jolly[n+1];
-    if(delta[p]==1){
-        printf("not Jolly\n");
-        break;
-    }
-    else{
-        delta[p]=1;
+    long long temp=1;
+    for(int i=0;i<10;i++){
+        printf("%lld\n",temp);
+        temp*=36;
     }
 }
-
-
-//
-/*
-n=4
-input =1,3,4,7
-delta 
-
-n=4
-input =1,3,5
-delta 2 2
-delat[2]=1;
-*/
+`
