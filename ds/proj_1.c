0c711d1d54c82fe8b348a9e6a40b8b12ef20f67b
Freddy Chuang <930727fre@gmail.com>
2023-09-21 17:58:00 +0800

    trivial

--- ds/proj_1.c
+++ ds/proj_1.c
@@ -66,16 +66,16 @@ void sync(int *src2, int deltaX,int deltaY,int (*maze)[1000]){
 
 int go(int a, int b, int c, int d){
     if(a-c==1){
-        return 0;
-    }
-    else if(a-c==-1){
-        return 2;
-    }
-    else if(b-d==1){
         return 1;
     }
-    else if(b-d==-1){
+    else if(a-c==-1){
         return 3;
+    }
+    else if(b-d==1){
+        return 0;
+    }
+    else if(b-d==-1){
+        return 2;
     }
     else{
         return -1;
@@ -92,7 +92,7 @@ void output(queue* current, int* src2, int (*maze)[1000]){
     return;
 }
 
-void bfs(int (*src), int (*src2), int (*dst), int maze[1000][1000]){
+void bfs(int (*src), int (*src2), int (*dst)[2], int maze[1000][1000]){
     int visited[1000][1000];
     for(int i=0;i<1000;i++){
         for(int j=0;j<1000;j++){
@@ -100,14 +100,26 @@ void bfs(int (*src), int (*src2), int (*dst), int maze[1000][1000]){
         }
     }
     queue *front=NULL, *last=NULL, *current=NULL;
-    push_back(src[1], src[0], &front, &last, NULL, maze);
+    push_back(src[0], src[1], &front, &last, NULL, maze);
     current=front;
     while(!empty(current)){
-        if(current->x==dst[1]&&current->y==dst[0]){ //if arrive the destination
+        if(current->x==dst[0][0]&&current->y==dst[0][1]){ //if arrive the destination
+            // printf("arrive %d %d\n",current->x, current->y);
+            dst[0][0]=-1;
+            dst[0][1]=-1;
             output(current, src2, maze);
             // printf("\n");
             return;
-        }else{
+        }
+        else if(current->x==dst[1][0]&&current->y==dst[1][1]){
+            // printf("arrive %d %d\n",current->x, current->y);
+            dst[1][0]=-1;
+            dst[1][1]=-1;
+            output(current, src2, maze);
+            // printf("\n");
+            return;            
+        }
+        else{
             if(maze[current->x+1][current->y]!=1&&visited[current->x+1][current->y]!=1){
                 push_back(current->x+1, current->y, &front, &last, &current, visited);
             }
@@ -129,19 +141,25 @@ int main(){
     int n, src[2][2], dst[2][2], maze[1000][1000];
     int nullSrc[]={-1,-1};
     scanf("%d",&n); // input size of the maze
-    for(int i=0;i<n;i++){
+    for(int i=n-1;i>=0;i--){
         for(int j=0;j<n;j++){
-            scanf("%d",&maze[i][j]); //input the content of maze
+            scanf("%d",&maze[j][i]); //input the content of maze
         }
     }
+    // for(int i=n-1;i>=0;i--){
+    //     for(int j=0;j<n;j++){
+    //         printf("%d ",maze[j][i]); //input the content of maze
+    //     }
+    //     printf("\n");
+    // }    
     for(int i=0;i<2;i++){
         scanf("%d %d",&src[i][0],&src[i][1]); //input the source
     }
     for(int i=0;i<2;i++){
         scanf("%d %d",&dst[i][0],&dst[i][1]); //input the destination
     }
-    bfs(src[0], src[1], dst[0], maze);
-    bfs(src[1], nullSrc, dst[1], maze);
+    bfs(src[0], src[1], dst, maze);
+    bfs(src[1], nullSrc, dst, maze);
     // clear(front);
 
 }
