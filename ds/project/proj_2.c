ecc8606a17b2556d4ac17b08799078e263a4b9df
frem2 <930727fre@gmail.com>
2023-10-26 21:32:00 +0800

    fix findDestination(), skipCount()

--- ds/project/proj_2.c
+++ ds/project/proj_2.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int *new, *old, *current, *updated, *unionSet;
-int updateCounter=0, roundCounter=1, operation=0, n;
+int *new, *old, *updated, *unionSet;
+int updateCounter=0, roundCounter=1, operation=0, n, current, temp;
 typedef struct Queue{
     struct Queue *next;
     int n;
 }queue;
-queue** q;
-queue** ans;
+queue **q, **ans, **prune, **shortcut;
 
 
 void push(queue** q, int value){
@@ -19,12 +18,14 @@ void push(queue** q, int value){
     }
     temp->next=(queue*)malloc(sizeof(queue));
     temp->next->n=value;
+    temp->next->next=NULL;
     return;
 }
 
 int top(queue** q){
     if((*q)->next==NULL){
         return INT_MIN;
+        printf("top empty\n");
     }
     else{
         return (*q)->next->n;
@@ -34,6 +35,7 @@ int top(queue** q){
 void pop(queue** q){
     queue* temp;
     if((*q)->next==NULL){
+        printf("pop empty\n");
         return;
     }
     else{
@@ -44,19 +46,43 @@ void pop(queue** q){
     }
 }
 
-int skipCount(int i){
-    int count=0, current=i, dest=new[i];
-    while(old[dest]==-1&&dest!=n-1){
-        dest=new[dest];
+int isEmpty(queue** q){
+    if((*q)->next==NULL){
+        return 1;
     }
-    printf("dest %d\n",dest);
+    else{
+        return 0;
+    }
+}
+
+int findDestination(int i){
+    while((old[i]==-1&&i!=n-1)||updated[i]==1){
+        i=new[i];
+    }
+    return i;
+}
+
+int skipCount(int i){
+    int count=0, current=i, dest=new[i], prevUpdated=0;
+    dest=findDestination(new[i]);
     do{
         if(current==-1){
             return -1;
         }
         current=old[current];
+
         if(old[current]!=-1&&new[current]!=-1&&current!=dest){
-            printf("current %d\n",current);
+            if(updated[current]==1){
+                if(prevUpdated==1){
+                    continue;
+                }
+                else{
+                    prevUpdated=1;
+                }
+            }
+            else{
+                prevUpdated=0;
+            }
             count++;
         }
     }while(current!=dest);
@@ -79,11 +105,51 @@ void dsuUnion(int a, int b){
     unionSet[temp2]=temp1;
 }
 
+void addShortcut(int node){
+    push(shortcut, node);
+    updated[node]=1;
+}
+
+void pruneFunction(void){
+    while(!isEmpty(shortcut)){
+        int current=top(shortcut);
+        pop(shortcut);
+        int dest=findDestination(new[current]);
+        printf("dest %d\n",dest);
+        if(skipCount(current)==-1){
+            updated[current]=1;
+            push(ans, current);
+        }
+        else{
+            current=old[current];
+            while(current!=dest){
+                if(old[current]!=-1&&new[current]!=-1&&updated[current]==0){
+                    push(prune, current);
+                }
+                current=old[current];
+            }
+        }
+    }
+    while(!isEmpty(prune)){
+        int temp=top(prune);
+        pop(prune);
+        push(ans, temp);
+        updated[temp]=1;
+        printf("pruned %d\n",temp);
+        updateCounter++;
+    }
+}
+
+void checkUpdated(void){
+    for(int i=0;i<n;i++){
+        printf("%d ", updated[i]);
+    }    
+}
+
 int main(){
     scanf("%d",&n);
     new=(int*)calloc(n,sizeof(int));
     old=(int*)calloc(n,sizeof(int));
-    current=(int*)calloc(n,sizeof(int));
     updated=(int*)calloc(n,sizeof(int));
     unionSet=(int*)calloc(n,sizeof(int));
     q=(queue**)malloc(sizeof(queue*));
@@ -91,15 +157,21 @@ int main(){
     (*q)->next=NULL;
     ans=(queue**)malloc(sizeof(queue*));
     *ans=(queue*)malloc(sizeof(queue));
-    (*ans)->next=NULL;    
+    (*ans)->next=NULL;
+    prune=(queue**)malloc(sizeof(queue*));
+    *prune=(queue*)malloc(sizeof(queue));
+    (*prune)->next=NULL;
+    shortcut=(queue**)malloc(sizeof(queue*));
+    *shortcut=(queue*)malloc(sizeof(queue));
+    (*shortcut)->next=NULL;
 
     for(int i=0;i<n;i++){// initialize union set
         unionSet[i]=i;
     }
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){// input
         scanf("%d",&old[i]);
     }
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){// input
         scanf("%d",&new[i]);
     }
 
@@ -115,14 +187,22 @@ int main(){
     push(ans,-1);
     roundCounter+=1;
 
-    // push(ans,0);
-    // push(ans,-1);    
-    // push(ans,1);
-    // push(ans,2);        
-    // push(ans,-1);
-    // push(ans,3);    
-    // push(ans,-1);    
-    // roundCounter+=3;
+    // while(updateCounter!=operation){
+
+    // }
+    addShortcut(0);
+    push(ans, -1);
+    updateCounter++;
+    roundCounter++;
+
+    pruneFunction();
+    push(ans, -1);
+    roundCounter++;
+    //
+    temp=3;
+    printf("skipCount %d %d\n",temp, skipCount(temp));        
+    checkUpdated();
+    printf("\n");
     for(int i=0;i<n;i++){
         if(old[i]!=-1&&new[i]==-1){
             push(ans,i);
@@ -130,31 +210,31 @@ int main(){
     }
     push(ans,-1);
     roundCounter+=1;
-    for(int i=0;i<n;i++){
-        if(new[i]!=-1&&old[i]!=-1){
-            printf("skip v%d %d\n",i,skipCount(i));
-        }
-    }
+    // for(int i=0;i<n;i++){
+    //     if(new[i]!=-1&&old[i]!=-1){
+    //         printf("skip v%d %d\n",i,skipCount(i));
+    //     }
+    // }
     printf("%d\n", roundCounter);
-    for(int i=0;i<roundCounter;i++){
-        while(1){
-            int temp=top(ans);
-            pop(ans);
-            if(temp==-1){
-                break;
-            }
-            else{
-                old[temp]=new[temp];
-            }
-        }
-        for(int j=0;j<n;j++){
-            printf("%d",old[j]);
-            if(j!=n-1){
-                printf(" ");
-            }
-        }
-        if(i!=roundCounter-1){
-            printf("\n");
-        }
-    }
+    // for(int i=0;i<roundCounter;i++){
+    //     while(1){
+    //         int temp=top(ans);
+    //         pop(ans);
+    //         if(temp==-1){
+    //             break;
+    //         }
+    //         else{
+    //             old[temp]=new[temp];
+    //         }
+    //     }
+    //     for(int j=0;j<n;j++){
+    //         printf("%d",old[j]);
+    //         if(j!=n-1){
+    //             printf(" ");
+    //         }
+    //     }
+    //     if(i!=roundCounter-1){
+    //         printf("\n");
+    //     }
+    // }
 }
