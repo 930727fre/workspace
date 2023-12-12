df0d042cbce0bf09b9ed6c0ef25b8c9975621c78
frem2 <930727fre@gmail.com>
2023-12-12 20:06:07 +0800

    i realized that i've implement queue to stack...

--- ds/project/proj_3/proj_3.c
+++ ds/project/proj_3/proj_3.c
@@ -7,23 +7,28 @@ typedef struct Link{ // adjacdncy list
     int val;
     struct Link* next;
 }link;
+typedef struct Stack{
+    struct Stack* prev;
+    int val;
+}stack;
 typedef struct Queue{
     struct Queue* prev;
+    struct Queue* next;
     int val;
 }queue;
 int nodes, links, timeSlots, requests;
 int request[NUM][3];
 int *memories;
 link **adjacencyList;
-int bfsResult[NUM]; // this array stores the number of hop each request takes according to bfs
-queue **bfsPath;
+int bfsResult[NUM][2]; // this array stores the number of hop each request takes according to bfs
+stack **bfsPath;
 
-int queueTop(queue** q){
-    return (*q)->val;
+int stackTop(stack** s){
+    return (*s)->val;
 }
 
-int queueEmpty(queue** q){
-    if((*q)==NULL){
+int stackEmpty(stack** s){
+    if((*s)==NULL){
         return 1;
     }
     else{
@@ -31,67 +36,72 @@ int queueEmpty(queue** q){
     }
 }
 
-void queuePop(queue** q){
-    queue* temp=(*q);
-    (*q)=(*q)->prev;
+void stackPop(stack** s){
+    stack* temp=(*s);
+    (*s)=(*s)->prev;
     free(temp);
     return;
 }
 
-void queuePush(queue** q, int value){
-    queue* ptr=(queue*)malloc(sizeof(queue));
-    ptr->prev=(*q);
+void stackPush(stack** s, int value){
+    stack* ptr=(stack*)malloc(sizeof(stack));
+    ptr->prev=(*s);
     ptr->val=value;
-    (*q)=ptr;
+    (*s)=ptr;
     return;
 }
 
-int bfsTraverse(int requestId, int* visited, queue** q){
+int bfsTraverse(int requestId, int* visited, stack** s){
     
-    int current=queueTop(q), temp;
+    int current=stackTop(s), temp;
     visited[current]=1;
-    queuePop(q);
+    stackPop(s);
     // printf("current=%d\n", current);
     if(current==request[requestId][2]){// destinaction reached
-        queuePush(&(bfsPath[requestId]), current);
+        stackPush(&(bfsPath[requestId]), current);
         return 1; //remember to minus 1
     }
     else{
         link* ptr=adjacencyList[current];
         while(ptr!=NULL){
             if(visited[ptr->val]==0){
-                queuePush(q, ptr->val);
+                stackPush(s, ptr->val);
             }
             ptr=ptr->next;
         }
-        temp=bfsTraverse(requestId, visited, q);
-        if(temp!=0){
-            queuePush(&(bfsPath[requestId]), current);
-            return temp+1;
+        if(stackEmpty(s)==0){
+            temp=bfsTraverse(requestId, visited, s);
+            if(temp!=0){
+                stackPush(&(bfsPath[requestId]), current);
+                return temp+1;
+            }
         }
-        else{
-            return 0;
-        }
+
     }
+    return 0;
 
 }
 
 int bfs(int requestId){
     int hop=0;
     int visisted[nodes];
-    queue* myQueue=NULL;
+    stack* myStack=NULL;
     memset(visisted, 0, sizeof(visisted)); //initialize the array
-    queuePush(&myQueue, request[requestId][1]);
-    return bfsTraverse(requestId, visisted, &myQueue);
+    printf("source %d destination %d\n",request[requestId][1], request[requestId][2]);
+    stackPush(&myStack, request[requestId][1]);
+    return bfsTraverse(requestId, visisted, &myStack);
 }
 
 int main(){
-    int tempA, tempB, temp;
+    int tempA, tempB, temp, tempArr[2];
     link* tempPtr;
     scanf("%d %d %d %d",&nodes, &links, &timeSlots, &requests);
     memories=(int*)malloc(nodes*sizeof(int));
     adjacencyList=(link**)malloc(nodes*sizeof(link*));
-    bfsPath=(queue**)malloc(requests*sizeof(queue*));
+    bfsPath=(stack**)malloc(requests*sizeof(stack*));
+    for(int i=0;i<NUM;i++){
+        bfsResult[i][0]=i;
+    }
     for(int i=0;i<nodes;i++){
         scanf("%d %d",&tempA, &memories[i]);
     }
@@ -135,15 +145,34 @@ int main(){
     }   
 
     for(int i=0;i<requests;i++){
-        bfsResult[i]=bfs(i)-1;
-        printf("#%d %d step\n", i, bfsResult[i]);
-        queue* ptr=bfsPath[i];
+        bfsResult[i][1]=bfs(i)-1;
+        // printf("#%d %d step\n", i, bfsResult[i][1]);
+        // stack* ptr=bfsPath[i];
+        // while(ptr!=NULL){
+        //     printf("%d ",ptr->val);
+        //     ptr=ptr->prev;
+        // }
+        // printf("\n");        
+    }
+    for(int i=0;i<requests;i++){
+        for(int j=0;j<requests-i;j++){
+            if(bfsResult[j][1]>bfsResult[j+1][1]){
+                temp=bfsResult[j][0];
+                bfsResult[j][0]=bfsResult[j+1][0];
+                bfsResult[j+1][0]=temp;
+                temp=bfsResult[j][1];
+                bfsResult[j][1]=bfsResult[j+1][1];
+                bfsResult[j+1][1]=temp;
+            }
+        }
+    }
+    for(int i=requests-1;i>=0;i--){
+        // printf("#%d %d step\n", i, bfsResult[i][1]);
+        stack* ptr=bfsPath[i];
         while(ptr!=NULL){
-            printf("%d ",ptr->val);
+            // printf("%d ",ptr->val);
             ptr=ptr->prev;
         }
-        printf("\n");        
-
-    }
-
+        // printf("\n");        
+    }    
 }
