8fe8f4f5d70a759ffd898c7cfc577bd68ddcb569
frem2 <930727fre@gmail.com>
2023-12-12 14:51:14 +0800

    queue checked

--- ds/project/proj_3/proj_3.c
+++ ds/project/proj_3/proj_3.c
@@ -1,14 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM 30000 // i guess the number of request may not exceed this number
 
 typedef struct Link{ // adjacdncy list
     int val;
     struct Link* next;
 }link;
+typedef struct Queue{
+    struct Queue* prev;
+    int val;
+}queue;
 int nodes, links, timeSlots, requests;
-int request[1000][3];
+int request[NUM][3];
 int *memories;
 link **adjacencyList;
+int bfsResult[NUM]; // this array stores the number of hop each request takes according to bfs
+
+int queueTop(queue** q){
+    return (*q)->val;
+}
+
+int queueEmpty(queue** q){
+    if((*q)==NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void queuePop(queue** q){
+    queue* temp=(*q);
+    (*q)=(*q)->prev;
+    free(temp);
+    return;
+}
+
+void queuePush(queue** q, int value){
+    queue* ptr=(queue*)malloc(sizeof(queue));
+    ptr->prev=(*q);
+    ptr->val=value;
+    (*q)=ptr;
+    return;
+}
+
+int bfs(int start, int dest){ // the return value is the number of hop
+    int hop=0;
+    queue* myQueue=NULL;
+    queuePush(&myQueue, 3);
+
+    return 3;
+}
 
 int main(){
     int tempA, tempB, temp;
@@ -16,7 +58,8 @@ int main(){
     scanf("%d %d %d %d",&nodes, &links, &timeSlots, &requests);
     memories=(int*)malloc(nodes*sizeof(int));
     adjacencyList=(link**)malloc(nodes*sizeof(link*));
-
+    bfs(0,1);
+    return 0;
     for(int i=0;i<nodes;i++){
         scanf("%d %d",&tempA, &memories[i]);
     }
