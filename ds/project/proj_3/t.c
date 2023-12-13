743bb0000f28ce7806cea9515a82f21ad5e9d1d2
frem2 <930727fre@gmail.com>
2023-12-13 21:32:36 +0800

    do some shitty optimization and AC

--- ds/project/proj_3/t.c
+++ ds/project/proj_3/t.c
@@ -1,15 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
+    if(bfsResult[index][1]>T){
+        return 0;  // Numerology is longer than timeSlot
+    }
+    else{
+        int arr[nodes], i=0, tempCapacity[nodes][T], time=T-1;
+        link* ptr=bfsPath[bfsResult[index][0]];
 
-typedef struct Link{
-    struct Link* prev;
-    int val;
-}link;
+        // Copy the capacity array to a temporary array
+        for(int i=0;i<nodes;i++){
+            for(int j=0;j<T;j++){
+                tempCapacity[i][j]=capacity[i][j];
+            }
+        }
 
-link** A;
+        // Extract the nodes from the BFS path
+        while(ptr!=NULL){
+            arr[i]=ptr->val;
+            i++;
+            ptr=ptr->prev;
+        }
 
-int main(){
-    link *ptr=NULL, *temp;
-    A=(link**)malloc(20*sizeof(link*));
-    A[7]
-}
+        // Check numerology constraint for each time step
+        for(int i=bfsResult[index][1]-1;i>0;i--){
+            if(i!=1){
+                if(check(arr[i],tempCapacity, time)&&check(arr[0],tempCapacity, time)){
+                    tempCapacity[arr[i]][time]++;
+                    tempCapacity[arr[0]][time]++;
+                }
+                else{
+                    return 0;
+                }
+                if(check(arr[i],tempCapacity, time-1)&&check(arr[i-1],tempCapacity, time-1)){
+                    tempCapacity[arr[i]][time-1]++;
+                    tempCapacity[arr[i-1]][time-1]++;
+                }
+                else{
+                    return 0;
+                }
+                if(check(arr[i],tempCapacity, time-2)&&check(arr[i-1],tempCapacity, time-2)){
+                    tempCapacity[arr[i]][time-2]++;
+                    tempCapacity[arr[i-1]][time-2]++;
+                } 
+                else{
+                    return 0;
+                }   
+            }
+            else{
+                if(check(arr[1],tempCapacity, time)&&check(arr[0],tempCapacity, time)){
+                    tempCapacity[arr[1]][time]++;
+                    tempCapacity[arr[0]][time]++;
+                } 
+                else{
+                    return 0;
+                }
+                if(check(arr[1],tempCapacity, time-1)&&check(arr[0],tempCapacity, time-1)){
+                    tempCapacity[arr[1]][time-1]++;
+                    tempCapacity[arr[0]][time-1]++;
+                } 
+                else{
+                    return 0;
+                } 
+            }
+                    
+            time--;
+        }
+       
+        // for(int j=T-1;j>=0;j--){
+        //     for(int i=0;i<nodes;i++){
+        //         printf("%d",tempCapacity[i][j]);
+        //     }
+        //     printf("\n");
+        // }   
+        // printf("\n");
+
+        // Copy the updated temporary capacity array back to the main capacity array
+        for(int i=0;i<nodes;i++){
+            for(int j=0;j<T;j++){
+                capacity[i][j]=tempCapacity[i][j];
+            }
+        }       
+        return 1;
+    }
