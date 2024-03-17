26eb134b2af7d6c7fa2cc3feb3f9975bdc541bde
frem2 <930727fre@gmail.com>
2024-03-17 14:33:00 +0800

    trivial

--- oop/test.cpp
+++ oop/test.cpp
@@ -1,14 +1,10 @@
+
 #include <iostream>
 
-void foo(int x, double y) {
-    std::cout << "foo(int, double)" << std::endl;
-}
+int main()
+{
+    // char str[3]={'a','b','c'};
+    // std::cout<<str;
 
-void foo(double x, int y) {
-    std::cout << "foo(double, int)" << std::endl;
-}
-
-int main() {
-    std::cout<<[1]<<endl;
     return 0;
 }
