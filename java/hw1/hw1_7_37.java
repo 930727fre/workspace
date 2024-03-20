cd6649e9320669c164592bf51d13a91964bf8b84
frem2 <930727fre@gmail.com>
2024-03-20 15:20:34 +0800

    trivial

---
+++ java/hw1/hw1_7_37.java
@@ -0,0 +1,6 @@
+public class hw1_7_37
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
--- oop/test.cpp
+++ oop/test.cpp
@@ -1,10 +1,14 @@
-
 #include <iostream>
+#include <string>
 
-int main()
-{
-    // char str[3]={'a','b','c'};
-    // std::cout<<str;
+int main() {
+    std::string str;
+    char arr[100];
+    std::getline(std::cin, str, '?');
+    std::cout<<std::cin.peek();
+    // std::cin.getline(arr, 80, '?');
+    // std::cout<<str<<std::endl;
+    // std::cout<<arr<<std::endl;
 
     return 0;
 }
